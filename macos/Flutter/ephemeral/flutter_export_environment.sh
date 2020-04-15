4586ba87edb8fefc6c3a8d8f5e54f6562485ea47
Aditya Anand <adityaanand13@live.com>
2020-04-15 15:22:26 +0530

    first commit

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aditya/Documents/IDE/Flutter/codelab2"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Library/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
